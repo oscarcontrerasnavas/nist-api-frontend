0d25a28b90caecd6beb1516cdb60c63735eb9ce9
Oscar Contreras <oscarcontrerasnavas@gmail.com>
2022-08-25 23:00:41 -0300

    Remove assumption on props, trying to fix build error.

--- components/substance-display.jsx
+++ components/substance-display.jsx
@@ -3,66 +3,73 @@ import parse from 'html-react-parser';
 function SubstanceDisplay({substance}) {
   const base_url = "https://nist-scrapyrt.herokuapp.com";
 
-  return (
-    <div className="row pt-4">
-        <div className="col-12">
-            <h2 className="text-capitalize text-center">{substance.name}</h2>
-            <div className="fw-bold text-primary text-center">
-                {parse(substance.formula.replace(/[0-9]+/ig, "<sub>$&</sub>"))}
+  if (substance) {
+
+      return (
+        <div className="row pt-4">
+            <div className="col-12">
+                <h2 className="text-capitalize text-center">{substance.name}</h2>
+                <div className="fw-bold text-primary text-center">
+                    {parse(substance.formula.replace(/[0-9]+/ig, "<sub>$&</sub>"))}
+                </div>
+            </div>
+            <div className="col-12 text-center my-4">
+                <img src={substance.image} alt="Structure" className="img-fluid" />
+            </div>
+            <div className="col-12">
+                <table className="table table-light">
+                    <tbody>
+                        <tr>
+                            <th scope='row'>
+                                CAS
+                            </th>
+                            <td>
+                                {substance.cas}
+                            </td>
+                        </tr>
+    
+                        <tr>
+                            <th scope='row'>
+                                Molecular weight
+                            </th>
+                            <td>
+                                {substance.molecular_weight}
+                            </td>
+                        </tr>
+    
+                        <tr>
+                            <th scope='row'>
+                                IUPAC Standard InChI
+                            </th>
+                            <td>
+                                {substance.iupac_std_inchi}
+                            </td>
+                        </tr>
+    
+                        <tr>
+                            <th scope='row'>
+                                IUPAC Standard InChIKey
+                            </th>
+                            <td>
+                                {substance.iupac_std_inchikey}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+    
+                <a href={
+                  base_url +
+                  `/crawl.json?spider_name=webbook_nist&start_requests=true&crawl_args={\"cas\":\"${substance.cas}\"}`
+                  } className="btn btn-primary my-4" target='_blank' rel="noreferrer">
+                    Full JSON response
+                  </a>
             </div>
         </div>
-        <div className="col-12 text-center my-4">
-            <img src={substance.image} alt="Structure" className="img-fluid" />
-        </div>
-        <div className="col-12">
-            <table className="table table-light">
-                <tbody>
-                    <tr>
-                        <th scope='row'>
-                            CAS
-                        </th>
-                        <td>
-                            {substance.cas}
-                        </td>
-                    </tr>
+      )
+  }
 
-                    <tr>
-                        <th scope='row'>
-                            Molecular weight
-                        </th>
-                        <td>
-                            {substance.molecular_weight}
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <th scope='row'>
-                            IUPAC Standard InChI
-                        </th>
-                        <td>
-                            {substance.iupac_std_inchi}
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <th scope='row'>
-                            IUPAC Standard InChIKey
-                        </th>
-                        <td>
-                            {substance.iupac_std_inchikey}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <a href={
-              base_url +
-              `/crawl.json?spider_name=webbook_nist&start_requests=true&crawl_args={\"cas\":\"${substance.cas}\"}`
-              } className="btn btn-primary my-4" target='_blank' rel="noreferrer">
-                Full JSON response
-              </a>
-        </div>
-    </div>
+  return (
+    <div></div>
   )
 }
 
--- components/substance-list.jsx
+++ components/substance-list.jsx
@@ -1,14 +1,21 @@
 import SubstanceCard from "./substance-card"
 
 function SubstanceList({substances}) {
+
+  if (substances) {
+    return (
+      <div className='row'>
+          <div className='col substances'>
+            {substances.map((substance)=>(
+              <SubstanceCard key={substance.cas} substance={substance} />
+            ))}
+          </div>
+      </div>
+    )
+  }
+
   return (
-    <div className='row'>
-        <div className='col substances'>
-          {substances.map((substance)=>(
-            <SubstanceCard key={substance.cas} substance={substance} />
-          ))}
-        </div>
-    </div>
+    <div></div>
   )
 }
 
